2c158f7feb922a67ffcc8f6d34866f96915f8052
Nadir GHARBI <nadirgharbi10@gmail.com>
2024-02-20 22:18:18 +0100

    bugfix and darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import axios, { AxiosResponse } from "axios";
-import { Spinner } from "flowbite-react";
+import { Spinner, Flowbite, DarkThemeToggle } from "flowbite-react";
 import { TodoList } from "./components/TodoList";
 
 /**
- * 
- * json-server : https://www.npmjs.com/package/json-server 
+ *
+ * json-server : https://www.npmjs.com/package/json-server
  * video json-server : https://www.youtube.com/watch?v=DPjtFcvU6bA
  */
 
 function App() {
-  const [todos, setTodos] = useState([]);
+	const [todos, setTodos] = useState([]);
 
-  useEffect(() => {
-    axios.get("http://localhost:3000/todos").then((result: AxiosResponse) => {
-      setTodos(result.data)
-    })
-    
-  }, [])
+	useEffect(() => {
+		axios.get("http://localhost:3000/todos").then((result: AxiosResponse) => {
+			setTodos(result.data);
+		});
+	}, []);
 
-  return (
-    <div className="p-5">
-      <div className="text-center text-zinc-800">
-        <h1 className="text-3xl font-bold">My Todo list</h1>
-      </div>
-      <div className="py-5 text-center">
-        {todos ? <TodoList todos={todos}/>: <Spinner />}
-      </div>
-    </div>
-  );
+	return (
+		<Flowbite>
+			<div className="p-5 bg-white dark:bg-zinc-900 h-screen">
+				<DarkThemeToggle />
+				<div className="text-center text-zinc-800 dark:text-white font-semibold">
+					<h1 className="text-3xl font-bold">My Todo list</h1>
+					<div className="py-5 text-center">{todos ? <TodoList todos={todos} /> : <Spinner />}</div>
+				</div>
+			</div>
+		</Flowbite>
+	);
 }
 
 export default App;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,76 +1,91 @@
 import React, { useState } from "react";
 import { Pagination, Table, Dropdown, TableRow } from "flowbite-react";
 import axios from "axios";
+import { FaDeleteLeft, FaPen } from "react-icons/fa6";
 
 interface TodosProps {
-  todos: Array<any>;
+	todos: Array<{
+		userId: number;
+		id: string; // Change this to string
+		title: string;
+		completed: boolean;
+	}>;
 }
 
-export const TodoList: React.FC<TodosProps> = ({ todos }) => { 
-  const itemsPerPage = 10;
-  const [currentPage, setCurrentPage] = useState<number>(1);
+export const TodoList: React.FC<TodosProps> = ({ todos }) => {
+	const itemsPerPage = 10;
+	const [currentPage, setCurrentPage] = useState<number>(1);
 
-  const onPageChange = (page: number) => setCurrentPage(page);
+	const onPageChange = (page: number) => setCurrentPage(page);
 
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const visibleTodos = todos.slice(startIndex, endIndex);
+	const startIndex = (currentPage - 1) * itemsPerPage;
+	const endIndex = startIndex + itemsPerPage;
+	const visibleTodos = todos.slice(startIndex, endIndex);
 
-  console.log(visibleTodos.length);
+	console.log(visibleTodos.length);
 
-  const handleEdit = () => {
-    // Gérer l'édition ici
-  };
+	const handleEdit = () => {
+		// Gérer l'édition ici
+	};
 
-  const handleDelete = (id: number) => {
-    axios
-      .delete(`http://localhost:3000/todos/${id}`)
-      .then((response) => {
-        // console.log(`Deleted post with ID ${id}`, response);
-        window.location.reload()
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
+	const handleDelete = (id: string) => {
+		console.log(`http://localhost:3000/todos/${id}`);
+		axios
+			.delete(`http://localhost:3000/todos/${id}`)
+			.then((response) => {
+				console.log(`Deleted post with ID ${id}`, response);
+				window.location.reload();
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	};
 
-  return (
-    <>
-      <Table striped>
-        <Table.Head>
-          <Table.HeadCell>Tâche N°</Table.HeadCell>
-          <Table.HeadCell>Nom</Table.HeadCell>
-          <Table.HeadCell>Etat</Table.HeadCell>
-          <Table.HeadCell>
-            <span className="sr-only">Edit</span>
-          </Table.HeadCell>
-        </Table.Head>
-        <Table.Body className="divide-y">
-          {visibleTodos.length > 0 ? visibleTodos.map((todo) => (
-            <Table.Row key={todo.id} className="bg-white border-b even:bg-zinc-100">
-              <Table.Cell scope="row" className="px-6 py-4 font-medium whitespace-nowrap">
-                {todo.id}
-              </Table.Cell>
-              <Table.Cell className="px-6 py-4">{todo.title}</Table.Cell>
-              <Table.Cell className="px-6 py-4">{todo.completed ? "Terminée" : "En cours"}</Table.Cell>
-              <Table.Cell className="px-6 py-4 left-0">
-                <Dropdown size="sm" label="Edit" dismissOnClick={false} color="gray">
-                  <Dropdown.Item>Modifier</Dropdown.Item>
-                  <Dropdown.Item
-                    onClick={() => handleDelete(todo.id)} // Assurez-vous que todo.id est de type number
-                    className="bg-red-100 hover:bg-red-200 text-red-500 font-bold"
-                  >
-                    Supprimer
-                  </Dropdown.Item>
-                </Dropdown>
-              </Table.Cell>
-            </Table.Row>
-          )) : <p className="text-center p-3 w-screen">Aucun Element</p>}
-        </Table.Body>
-      </Table>
-      <div className="text-center my-6">
-        <Pagination currentPage={currentPage} totalPages={todos.length / 10} onPageChange={onPageChange} showIcons />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Table striped>
+				<Table.Head>
+					<Table.HeadCell>Tâche N°</Table.HeadCell>
+					<Table.HeadCell>Nom</Table.HeadCell>
+					<Table.HeadCell>Etat</Table.HeadCell>
+					<Table.HeadCell>
+						<span className="sr-only">Edit</span>
+					</Table.HeadCell>
+				</Table.Head>
+				<Table.Body className="divide-y">
+					{visibleTodos.length > 0 ? (
+						visibleTodos.map((todo) => (
+							<Table.Row key={todo.id} className="bg-white border-b even:bg-zinc-100">
+								<Table.Cell scope="row" className="px-6 py-4 font-medium whitespace-nowrap">
+									{todo.id}
+								</Table.Cell>
+								<Table.Cell className="px-6 py-4">{todo.title}</Table.Cell>
+								<Table.Cell className="px-6 py-4">{todo.completed ? "Terminée" : "En cours"}</Table.Cell>
+								<Table.Cell className="px-6 py-4 left-0">
+									<Dropdown size="sm" label="Edit" dismissOnClick={false} color="gray">
+										<Dropdown.Item>
+											<FaPen />
+											<span className="ps-3">Modifier</span>
+										</Dropdown.Item>
+										<Dropdown.Item
+											onClick={() => handleDelete(todo.id)} // Assurez-vous que todo.id est de type number
+										>
+											<FaDeleteLeft />
+											<span className="ps-3">Supprimer</span>
+										</Dropdown.Item>
+									</Dropdown>
+								</Table.Cell>
+							</Table.Row>
+						))
+					) : (
+						<p className="text-center p-3 w-screen">Aucun Element</p>
+					)}
+				</Table.Body>
+			</Table>
+			<div className="text-center my-6">
+				{/* <Pagination currentPage={currentPage} totalPages={todos.length + 1 / 10} onPageChange={onPageChange} showIcons /> */}
+				<Pagination currentPage={currentPage} totalPages={Math.ceil(todos.length / itemsPerPage)} onPageChange={onPageChange} showIcons />
+			</div>
+		</>
+	);
 };
