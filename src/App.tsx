1fd74f23f982f8d4446eb0fe154a1bf868c85d45
Nadir GHARBI <nadirgharbi10@gmail.com>
2024-02-21 23:41:58 +0100

    add sort and filter

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,6 @@ function App() {
 	const currentYear = new Date();
 	const [todos, setTodos] = useState([]);
 
-	const handleAdd = () => {
-		// Gérer l'édition ici
-	};
-
 	useEffect(() => {
 		axios.get("http://localhost:3000/todos").then((result: AxiosResponse) => {
 			setTodos(result.data);
@@ -29,26 +25,13 @@ function App() {
 						<div>
 							<h1 className="text-3xl font-bold">My Todo list</h1>
 						</div>
-						<div className="flex justify-end">
-							<Dropdown size="sm" label="Action" dismissOnClick={false} color="gray">
-								<Dropdown.Item>
-									<span className="ps-3">Ajouter une tâche</span>
-								</Dropdown.Item>
-								<Dropdown.Item
-									onClick={handleAdd} // Assurez-vous que todo.id est de type number
-								>
-									<span className="ps-3 text-red-500 dark:text-red-400">Supprimer toutes les tâches</span>
-								</Dropdown.Item>
-							</Dropdown>
-						</div>
-
 						<div className="py-5 flex flex-col items-center">{todos ? <TodoList todos={todos} /> : <Spinner />}</div>
 					</div>
 				</Flowbite>
 			</div>
 			{/* Footer */}
 			<div className="fixed flex flex-col gap-1 justify-center items-center bottom-0 p-3 bg-zinc-50 dark:bg-zinc-800 text-zinc-800 dark:text-zinc-100 border-t dark:border-t-white/10 w-full">
-				<small>{currentYear.getFullYear()} &copy; Nadir GHARBI</small>
+				<small>{currentYear.getFullYear()} &copy; Nadir GHARBI - v0.1</small>
 				<div className="flex gap-2">
 					<a href="/">
 						<FaDiscord size={20} className="text-zinc-300 dark:text-zinc-600 hover:text-[#7289da] dark:hover:text-[#7289da]" />
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,103 +1,170 @@
 import React, { useState } from "react";
-import { Pagination, Table, Dropdown, Button } from "flowbite-react";
+import { Pagination, Table, Dropdown, Button, TextInput, Spinner, Badge } from "flowbite-react";
 import axios from "axios";
 import { FaDeleteLeft, FaPen } from "react-icons/fa6";
-import { BiPlus } from "react-icons/bi";
-import notFound from '../assets/not-found.svg'
+import { BiPlus, BiSolidTrash, BiSortUp, BiSortDown, BiSearch, BiFilter, BiSortAZ } from "react-icons/bi";
+import { HiCheck, HiClock } from "react-icons/hi";
+import { FaTasks  } from "react-icons/fa";
+import notFound from "../assets/not-found.svg";
 
 interface TodosProps {
-  todos: Array<{
-    userId: number;
-    id: string; // Change this to string
-    title: string;
-    completed: boolean;
-  }>;
+	todos: Array<{
+		userId: number;
+		id: string; // Change this to string
+		title: string;
+		completed: boolean;
+	}>;
 }
 
 export const TodoList: React.FC<TodosProps> = ({ todos }) => {
-  const itemsPerPage = 10;
-  const [currentPage, setCurrentPage] = useState<number>(1);
+	const itemsPerPage = 10;
+	const [currentPage, setCurrentPage] = useState<number>(1);
 
-  const onPageChange = (page: number) => setCurrentPage(page);
+	const onPageChange = (page: number) => setCurrentPage(page);
 
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const visibleTodos = todos.slice(startIndex, endIndex);
+	const startIndex = (currentPage - 1) * itemsPerPage;
+	const endIndex = startIndex + itemsPerPage;
+	const visibleTodos = todos.slice(startIndex, endIndex);
 
-  console.log(visibleTodos.length);
+	console.log(visibleTodos.length);
 
-  const handleEdit = () => {
-    // Gérer l'édition ici
-  };
+	const handleEdit = () => {
+		// Gérer l'édition ici
+	};
 
-  const handleDelete = (id: string) => {
-    console.log(`http://localhost:3000/todos/${id}`);
-    axios
-      .delete(`http://localhost:3000/todos/${id}`)
-      .then((response) => {
-        console.log(`Deleted post with ID ${id}`, response);
-        window.location.reload();
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
+	const handleAdd = () => {
+		// Gérer l'édition ici
+	};
 
-  return (
-    <>
-      {visibleTodos.length > 0 ? (
-        <>
-          {" "}
-          <Table striped>
-            <Table.Head>
-              <Table.HeadCell>Tâche N°</Table.HeadCell>
-              <Table.HeadCell>Nom</Table.HeadCell>
-              <Table.HeadCell>Etat</Table.HeadCell>
-              <Table.HeadCell>
-                <span className="sr-only">Edit</span>
-              </Table.HeadCell>
-            </Table.Head>
-            <Table.Body className="divide-y">
-              {visibleTodos.map((todo) => (
-                <Table.Row key={todo.id} className="bg-white border-b even:bg-zinc-100">
-                  <Table.Cell scope="row" className="px-6 py-4 font-medium whitespace-nowrap">
-                    {todo.id}
-                  </Table.Cell>
-                  <Table.Cell className="px-6 py-4">{todo.title}</Table.Cell>
-                  <Table.Cell className="px-6 py-4">{todo.completed ? "Terminée" : "En cours"}</Table.Cell>
-                  <Table.Cell className="px-6 py-4 left-0">
-                    <Dropdown size="sm" label="Edit" dismissOnClick={false} color="gray">
-                      <Dropdown.Item>
-                        <FaPen />
-                        <span className="ps-3">Modifier</span>
-                      </Dropdown.Item>
-                      <Dropdown.Item
-                        className="text-red-500 dark:text-red-400"
-                        onClick={() => handleDelete(todo.id)} // Assurez-vous que todo.id est de type number
-                      >
-                        <FaDeleteLeft />
-                        <span className="ps-3">Supprimer</span>
-                      </Dropdown.Item>
-                    </Dropdown>
-                  </Table.Cell>
-                </Table.Row>
-              ))}
-            </Table.Body>
-          </Table>
-          <div className={`text-center my-6 `}>
-            <Pagination currentPage={currentPage} totalPages={Math.ceil(todos.length / itemsPerPage)} onPageChange={onPageChange} showIcons />
-          </div>
-        </>
-      ) : (
-        <>
-          <p className="text-center">Aucun élement trouvés...</p>
-          <img src={notFound} width={420} className="py-5" />
-          <Button color="gray">
-            <BiPlus />
-            Ajouter une nouvelle tâche
-          </Button>
-        </>
-      )}
-    </>
-  );
+	const handleDelete = (id: string) => {
+		axios
+			.delete(`http://localhost:3000/todos/${id}`)
+			.then((response) => {
+				console.log(`Deleted post with ID ${id}`, response);
+				window.location.reload();
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	};
+
+	return (
+		<>
+			{visibleTodos.length > 0 ? (
+				<>
+					{/* Add New Task */}
+					<div className="flex justify-between w-1/2 gap-1">
+						<Button color="gray">
+							<BiPlus className="me-1" />
+							Ajouter une tâche
+						</Button>
+						{/* <Dropdown size="sm" label="Action" dismissOnClick={false} color="gray" className="grid" placement="right">
+							<Dropdown.Item>
+								<BiPlus />
+								<span className="ps-3">Ajouter une tâche</span>
+							</Dropdown.Item>
+							<Dropdown.Item
+								className="text-red-500 dark:text-red-400"
+								onClick={handleAdd} // Assurez-vous que todo.id est de type number
+							>
+								<BiSolidTrash />
+								<span className="ps-3">Supprimer toutes les tâches</span>
+							</Dropdown.Item>
+						</Dropdown> */}
+						<div className="flex gap-1">
+							<TextInput type="text" icon={BiSearch} placeholder="Rechercher" />
+							{/* Sort */}
+							<Dropdown size="sm" label="Trier" dismissOnClick={false} color="gray">
+								<Dropdown.Item>
+									<BiSortAZ />
+									<span className="ps-3">Par alphabétique</span>
+								</Dropdown.Item>
+								<Dropdown.Item>
+									<FaTasks />
+									<span className="ps-3">Par état</span>
+								</Dropdown.Item>
+							</Dropdown>
+							{/* Filter  */}
+							<Dropdown size="sm" label="Filtrer" dismissOnClick={false} color="gray">
+								<Dropdown.Item>
+									<HiCheck />
+									<span className="ps-3">Terminée</span>
+								</Dropdown.Item>
+								<Dropdown.Item>
+									<HiClock />
+									<span className="ps-3">En cours</span>
+								</Dropdown.Item>
+							</Dropdown>
+						</div>
+					</div>
+
+					{/* Todolist */}
+					<div className="pt-5 w-1/2">
+						<Table striped>
+							<Table.Head>
+								<Table.HeadCell> </Table.HeadCell>
+								<Table.HeadCell>Nom</Table.HeadCell>
+								<Table.HeadCell>Etat</Table.HeadCell>
+								<Table.HeadCell>
+									<span className="sr-only">Edit</span>
+								</Table.HeadCell>
+							</Table.Head>
+							<Table.Body className="divide-y">
+								{visibleTodos.map((todo) => (
+									<Table.Row key={todo.id} className="bg-white border-b even:bg-zinc-100">
+										<Table.Cell scope="row" className="px-6 py-4 font-medium whitespace-nowrap">
+											{todo.id}
+										</Table.Cell>
+										<Table.Cell className="px-6 py-4">{todo.title}</Table.Cell>
+										<Table.Cell className="px-6 py-4">
+											{todo.completed ? (
+												<Badge color="success" icon={HiCheck} className="w-20">
+													Terminée
+												</Badge>
+											) : (
+												<Badge color="warning" icon={HiClock} className="w-20">
+													En cours
+												</Badge>
+											)}
+
+											{/* <span>{todo.completed ? "Terminée" : "En cours"}</span> */}
+										</Table.Cell>
+										<Table.Cell className="px-6 py-4 left-0">
+											<Dropdown size="sm" label="Actions" dismissOnClick={false} color="gray">
+												<Dropdown.Item>
+													<FaPen />
+													<span className="ps-3">Modifier</span>
+												</Dropdown.Item>
+												<Dropdown.Item
+													className="text-red-500 dark:text-red-400"
+													onClick={() => handleDelete(todo.id)} // Assurez-vous que todo.id est de type number
+												>
+													<FaDeleteLeft />
+													<span className="ps-3">Supprimer</span>
+												</Dropdown.Item>
+											</Dropdown>
+										</Table.Cell>
+									</Table.Row>
+								))}
+							</Table.Body>
+						</Table>
+						<div className={`text-center my-6 `}>
+							<Pagination currentPage={currentPage} totalPages={Math.ceil(todos.length / itemsPerPage)} onPageChange={onPageChange} showIcons />
+						</div>
+					</div>
+				</>
+			) : (
+				visibleTodos.length ?? (
+					<>
+						<p className="text-center">Aucun élement trouvés...</p>
+						<img src={notFound} width={420} className="py-5" />
+						<Button color="gray">
+							<BiPlus />
+							Ajouter une nouvelle tâche
+						</Button>
+					</>
+				)
+			)}
+		</>
+	);
 };
